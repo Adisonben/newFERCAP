70ed7eca2651c9fdf78f83e4d04d8a806022ea46
Adison Wonglakhon <131834926+Adisonben@users.noreply.github.com>
2025-02-08 17:06:49 +0700

    Update Login.jsx

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -86,7 +86,7 @@ export default function Login({ status, canResetPassword, error }) {
                     )}
                 </div>
 
-                <div className="mt-4 flex items-center justify-end">
+                <div className="mt-4 flex items-center justify-end gap-x-2">
                     {/* {canResetPassword && (
                         <Link
                             href={route('password.request')}
@@ -95,7 +95,14 @@ export default function Login({ status, canResetPassword, error }) {
                             Forgot your password?
                         </Link>
                     )} */}
-
+                    <p>Don't have an account yet ?</p>
+                    <Link
+                        href={route("register")}
+                        className="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:text-gray-400 dark:hover:text-gray-100 dark:focus:ring-offset-gray-800"
+                    >
+                        Sign up
+                    </Link>
+                    <p>or</p>
                     <PrimaryButton className="ms-4" disabled={processing}>
                         Log in
                     </PrimaryButton>
