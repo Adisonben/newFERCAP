5b7336c1499224f474a93354f8e08c322049a8c0
Adison Wonglakhon <131834926+Adisonben@users.noreply.github.com>
2025-02-11 15:45:46 +0700

    Update RegisteredUserController.php

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -34,7 +34,7 @@ class RegisteredUserController extends Controller
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
-            'role' => 'required|string|max:255',
+            'role' => 'required|max:255',
             'email' => 'required|string|email|max:255|unique:'.User::class,
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
